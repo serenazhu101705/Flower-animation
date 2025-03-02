f633a21dbb8fd1b96ce56341d2e34db5a571640a
Serena Rui Zhu <serenazhu1017@gmail.com>
2025-03-01 19:35:13 -0500

    Update audio.js

--- js/audio.js
+++ js/audio.js
@@ -1,9 +1,5 @@
 let myAudio = new Audio("./img/I GUESS I'M IN LOVE.mp3");
 
 document.addEventListener('DOMContentLoaded', (event) => {
-    document.getElementById('play-audio').addEventListener('click', () => {
-        myAudio.play();
-        // Optionally hide the button after playing
-        document.getElementById('play-audio').style.display = 'none';
-    });
+    myAudio.play();
 });
