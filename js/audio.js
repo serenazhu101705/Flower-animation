9a706c56592c8a7d70b28513f2be503c011d14b4
Serena Rui Zhu <serenazhu1017@gmail.com>
2025-03-01 19:26:57 -0500

    Update audio.js

--- js/audio.js
+++ js/audio.js
@@ -1,4 +1,4 @@
-let myAudio = new Audio("https://cdn.pixabay.com/audio/2024/02/14/audio_b9bc3934cc.mp3"); // Create an Audio object with the file 'Ceylon.mp3'
+let myAudio = new Audio("./img/I GUESS I'M IN LOVE.mp3"); // Create an Audio object with the new file
 
 window.onload = function() {
     setInterval(GameLoop, 1000 / 10); // Start the game loop when the window is loaded
