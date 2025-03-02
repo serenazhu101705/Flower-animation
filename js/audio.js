883b0814591bcfc299d5e50c61daeef9f8db44d3
Serena Rui Zhu <serenazhu1017@gmail.com>
2025-03-01 23:13:53 -0500

    Update audio.js

--- js/audio.js
+++ js/audio.js
@@ -1,20 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
   var audio = new Audio("./img/Guilty as Sin_.mp3");
-  audio.loop = true; // Add this line to loop the song
+  audio.loop = true; // Loop the song
+
   var playButton = document.getElementById("play-music-btn");
+  
+  if (playButton) {
+    var playPromise = audio.play();
 
-  var playPromise = audio.play();
+    if (playPromise !== undefined) {
+      playPromise.catch(() => {
+        playButton.style.display = "block";
+      });
+    }
 
-  if (playPromise !== undefined) {
-    playPromise.catch(() => {
-      playButton.style.display = "block";
+    playButton.addEventListener("click", function () {
+      audio.play();
+      playButton.style.display = "none";
     });
   }
-
-  playButton.addEventListener("click", function () {
-    audio.play();
-    playButton.style.display = "none";
-  });
 
   // Function to stop the music
   window.stopMusic = function() {
